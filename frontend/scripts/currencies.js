5be30b26d50993afd47b83120717a20cfdbe19a1
jos-cabrera <joseacabrera@protonmail.com>
2025-07-20 19:03:10 -0300

    async/await

--- frontend/scripts/currencies.js
+++ frontend/scripts/currencies.js
@@ -3,31 +3,30 @@ import { createCurrency } from './modules/currency-ui.js';
 import { setCurrencies } from './modules/conversor.js';
 import { addCurrencyButtons } from './modules/currency-buttons.js';
 
-function loadCurrencies() {
-	fetchCurrencies()
-		.then((data) => {
-			const currencyList = document.querySelector('.currencies');
-			if (!currencyList) return;
+async function loadCurrencies() {
+	try {
+		const data = await fetchCurrencies();
+		const currencyList = document.querySelector('.currencies');
+		if (!currencyList) return;
 
-			setCurrencies(data);
+		setCurrencies(data);
 
-			data.forEach((currency) => {
-				currencyList.insertBefore(
-					createCurrency(currency),
-					currencyList.firstChild
-				);
-			});
-
-			addCurrencyButtons();
-
-			const observer = new MutationObserver(addCurrencyButtons);
-			observer.observe(document.getElementById('currencies'), {
-				childList: true,
-			});
-		})
-		.catch((error) => {
-			console.error('Failed to load data:', error);
+		data.forEach((currency) => {
+			currencyList.insertBefore(
+				createCurrency(currency),
+				currencyList.firstChild
+			);
 		});
+
+		addCurrencyButtons();
+
+		const observer = new MutationObserver(addCurrencyButtons);
+		observer.observe(document.getElementById('currencies'), {
+			childList: true,
+		});
+	} catch (error) {
+		console.error('Failed to load data:', error);
+	}
 }
 
 document.addEventListener('DOMContentLoaded', loadCurrencies);
