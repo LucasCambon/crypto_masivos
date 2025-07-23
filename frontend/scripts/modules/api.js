cf534a407bcd6f79c0f6db6dd1d7ee76c86ec1a9
Tomás <iglesiastomas99@gmail.com>
2025-07-22 21:48:49 -0300

    set api base url for prod and dev

--- frontend/scripts/modules/api.js
+++ frontend/scripts/modules/api.js
@@ -1,6 +1,10 @@
+const API_BASE_URL = window.location.hostname === 'localhost' 
+  ? 'http://localhost:3000'
+  : 'https://crypto-masivos.onrender.com';
+
 export async function fetchCurrencies() {
 	try {
-		const response = await fetch('/api/v1/currencies/list');
+		const response = await fetch(`${API_BASE_URL}/api/v1/currencies/list`);
 		const result = await response.json();
 		return result.data;
 	} catch (error) {
