171231d7190030088e841abb9249ad2aae1a7c82
jos-cabrera <joseacabrera@protonmail.com>
2025-07-23 14:24:49 -0300

    change: select users view by default

--- frontend/scripts/dashboard.js
+++ frontend/scripts/dashboard.js
@@ -5,11 +5,16 @@ document.addEventListener('DOMContentLoaded', () => {
 	const usersBtn = document.getElementById('users-btn');
 	const currenciesBtn = document.getElementById('currencies-btn');
 
+	usersBtn.classList.add('active-btn');
+	currenciesBtn.classList.remove('active-btn');
+	showUsers();
+
 	usersBtn.addEventListener('click', () => {
 		usersBtn.classList.add('active-btn');
 		currenciesBtn.classList.remove('active-btn');
 		showUsers();
 	});
+
 	currenciesBtn.addEventListener('click', () => {
 		currenciesBtn.classList.add('active-btn');
 		usersBtn.classList.remove('active-btn');
